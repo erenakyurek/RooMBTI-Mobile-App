697de5d9f237447c02ec0004beae200f121efbe2
darikan <dogubahaa@gmail.com>
2025-04-25 00:44:44 +0300

    Chat Linkage DONE

--- app/src/main/java/com/example/roombti/PersonInspectActivity.kt
+++ app/src/main/java/com/example/roombti/PersonInspectActivity.kt
@@ -1,6 +1,8 @@
 package com.example.roombti
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -23,5 +25,9 @@ class PersonInspectActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    fun open_chat(view: View) {
+        val intent = Intent(this, DMPageActivity::class.java)
+        startActivity(intent)
     }
 }
