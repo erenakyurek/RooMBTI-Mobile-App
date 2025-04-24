39ecaf059f130838baee36747bb8e9483f13ee6f
darikan <dogubahaa@gmail.com>
2025-04-24 16:41:46 +0300

    common page updated

---
+++ app/src/main/java/com/example/roombti/CommonActivity.kt
@@ -0,0 +1,36 @@
+package com.example.roombti
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.content.Intent
+import com.example.roombti.databinding.ActivityCommonBinding
+
+class CommonActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCommonBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityCommonBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun looking_roommate(view: View) {
+
+    }
+
+    fun looking_home(view: View) {
+
+    }
+}
