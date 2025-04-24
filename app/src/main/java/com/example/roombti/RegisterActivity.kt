9f3028392782f256d396460693894852f55a0a0c
darikan <dogubahaa@gmail.com>
2025-04-24 16:36:53 +0300

    view binding added

--- app/src/main/java/com/example/roombti/RegisterActivity.kt
+++ app/src/main/java/com/example/roombti/RegisterActivity.kt
@@ -1,20 +1,34 @@
 package com.example.roombti
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.roombti.databinding.ActivityIntroBinding
+import com.example.roombti.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_register)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.register)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun next_register(view: View) {
+        val intent = Intent(this, CommonActivity::class.java)
+        startActivity(intent)
     }
 }
